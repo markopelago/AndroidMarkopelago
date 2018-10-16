e6ee347b3158eed2f9b8ad4eb3bd2148d77e56c6
Warih Hadi Suryono <warih_suryono@yahoo.com>
2018-10-16 20:10:05 +0700

    notifications

--- app/src/main/java/com/markopelago/MainActivity.kt
+++ app/src/main/java/com/markopelago/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.markopelago
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.media.RingtoneManager
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
-import kotlin.system.exitProcess
+import okhttp3.*
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+
+var TOKEN = ""
 
 fun isNetworkAvailable(context: Context): Boolean {
     val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -17,8 +30,92 @@ fun isNetworkAvailable(context: Context): Boolean {
     return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting
 }
 
+fun readWebView(context: Context,webview: WebView) {
+    val currentUrl = webview.originalUrl
+    if(currentUrl != null) {
+        if (currentUrl.toString().contains("set_apps_token=")) {
+            val temp = currentUrl.split("set_apps_token=")
+            TOKEN = temp[1]
+            if(TOKEN!="") {
+                val path = context.getFilesDir()
+                val filename = File(path.toString() + "/bWFya29wZWxhZ28=.dat")
+                filename.writeText(TOKEN)
+                webview!!.loadUrl(currentUrl.replace("set_apps_token=" + TOKEN,""))
+            }
+        }
+    }
+    Handler().postDelayed({ readWebView(context,webview) }, 1000)
+}
+
+fun readNotification(context: Context){
+    if(TOKEN != "") {
+        val url = context.getResources().getString(R.string.SERVER_HOST) + "read_notification.php?token="+TOKEN
+        val client = OkHttpClient()
+        val request = Request.Builder().url(url).build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val responseGET = response.body()!!.string()
+                    if (responseGET != "") {
+                        val responseGETs = responseGET.split("]]]")
+                        if (responseGETs.size > 0) {
+                            responseGETs.forEach {
+                                val responses = it.split("|||")
+                                if (responses.size > 1) {
+                                    showNotification(context.applicationContext, context.getResources().getString(R.string.app_name), responses[1].toString(), responses[0].toInt())
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    }
+    Handler().postDelayed({ readNotification(context) }, 3000)
+}
+
+fun showNotification(context: Context,title:String,message:String,mNotificationId: Int = 1000){
+    lateinit var mNotification: Notification
+    var notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    val notifyIntent = Intent(context, MainActivity::class.java)
+
+    notifyIntent.putExtra("mNotified", true)
+    notifyIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+    val pendingIntent = PendingIntent.getActivity(context, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+    val res = context.resources
+    val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        mNotification = Notification.Builder(context,"com.markopelago.EXTRA_CHANNEL_ID")
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(res, R.mipmap.ic_launcher))
+                .setContentTitle(title)
+                .setStyle(Notification.BigTextStyle()
+                        .bigText(message))
+                .setContentText(message).build()
+    } else {
+        mNotification = Notification.Builder(context)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(res, R.mipmap.ic_launcher))
+                .setPriority(Notification.PRIORITY_MAX)
+                .setContentTitle(title)
+                .setStyle(Notification.BigTextStyle()
+                        .bigText(message))
+                .setSound(uri)
+                .setContentText(message).build()
+
+    }
+    notificationManager.notify(mNotificationId, mNotification)
+}
+
 class MainActivity : AppCompatActivity() {
-    private val SERVER_HOST = "https://www.markopelago.com/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,15 +127,19 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         }
-
         if (isNetworkAvailable(this@MainActivity)) {
-            webview!!.loadUrl(SERVER_HOST + "kanari/")
+            webview!!.loadUrl(this@MainActivity.getResources().getString(R.string.SERVER_HOST) + "android_apps.php")
+            val path = this@MainActivity.getFilesDir()
+            val filename = File(path.toString() + "/bWFya29wZWxhZ28=.dat")
+            if(filename.exists()){
+                TOKEN = FileInputStream(filename).bufferedReader().use { it.readText() }
+            }
+            readWebView(this@MainActivity,webview)
+            readNotification(this@MainActivity)
         } else {
             Toast.makeText(this@MainActivity,"Please check your internet connection, then restart this App",Toast.LENGTH_SHORT).show()
         }
-
     }
-
     override fun onBackPressed() {
         super.onBackPressed()
         moveTaskToBack(true)

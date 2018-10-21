7f57606b0e3aa79e99720e897e3b1a8fbb5e6862
Warih Hadi Suryono <warih_suryono@yahoo.com>
2018-10-21 15:45:41 +0700

    success riset upload image

---
+++ app/src/main/java/com/markopelago/MyWb.java
@@ -0,0 +1,249 @@
+package com.markopelago;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import java.io.File;
+
+public class MyWb extends Activity {
+    /** Called when the activity is first created. */
+
+    WebView web;
+    ProgressBar progressBar;
+
+    private ValueCallback<Uri> mUploadMessage;
+    private ValueCallback<Uri[]> mUploadMessages;
+    private final static int FILECHOOSER_RESULTCODE=1;
+    private final static int KITKAT_RESULTCODE = 2;
+    private Uri mCapturedImageURI = null;
+
+    WebChromeClient chromeClient = new WebChromeClient(){
+        // For Android 3.0+
+        public void openFileChooser(ValueCallback<Uri> uploadMsg) { /* Default code */ }
+
+        // For Android 3.0+
+        public void openFileChooser( ValueCallback uploadMsg, String acceptType ) { /* Default code */ }
+
+        //For Android 4.1, also default but it'll be as example
+        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture){
+            mUploadMessage = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("*/*");
+            MyWb.this.startActivityForResult(Intent.createChooser(i, "File Chooser"), MyWb.FILECHOOSER_RESULTCODE);
+
+        }
+
+        // The new code
+        public void showPicker( ValueCallback<Uri> uploadMsg ){
+            // Here is part of the issue, the uploadMsg is null since it is not triggered from Android
+            mUploadMessage = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("*/*");
+            MyWb.this.startActivityForResult(Intent.createChooser(i, "File Chooser"), MyWb.KITKAT_RESULTCODE);
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        web = (WebView) findViewById(R.id.webview01);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+
+        web = new WebView(this);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.addJavascriptInterface(chromeClient, "jsi" );
+        web.getSettings().setAllowFileAccess(true);
+        web.getSettings().setAllowContentAccess(true);
+        web.clearCache(true);
+        web.loadUrl("http://192.168.0.27/markopelago/api/android_apps.php");
+        web.setWebViewClient(new myWebClient());
+
+        web.setWebChromeClient(new WebChromeClient() {
+
+            // openFileChooser for Android 3.0+
+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType){
+                mUploadMessage = uploadMsg;
+                openImageChooser();
+            }
+
+            // For Lollipop 5.0+ Devices
+
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+                mUploadMessages = filePathCallback;
+                openImageChooser();
+                return true;
+            }
+
+            // openFileChooser for Android < 3.0
+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg){
+                openFileChooser(uploadMsg, "");
+            }
+
+            //openFileChooser for other Android versions
+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                openFileChooser(uploadMsg, acceptType);
+            }
+        });
+
+        setContentView(web);
+    }
+
+    private void openImageChooser() {
+        try {
+            File imageStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "FolderName");
+            if (!imageStorageDir.exists()) {
+                imageStorageDir.mkdirs();
+            }
+            File file = new File(imageStorageDir + File.separator + "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+            mCapturedImageURI = Uri.fromFile(file);
+
+            final Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);
+
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("image/*");
+
+            Intent chooserIntent = Intent.createChooser(i, "Image Chooser");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Parcelable[]{captureIntent});
+
+            startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if(requestCode==FILECHOOSER_RESULTCODE) {
+
+            if (null == mUploadMessage && null == mUploadMessages) {
+                return;
+            }
+
+            if (null != mUploadMessage) {
+                handleUploadMessage(requestCode, resultCode, intent);
+
+            } else if (mUploadMessages != null) {
+                handleUploadMessages(requestCode, resultCode, intent);
+            }
+        }
+    }
+
+    private void handleUploadMessage(int requestCode, int resultCode, Intent intent) {
+        Uri result = null;
+        try {
+            if (resultCode != RESULT_OK) {
+                result = null;
+            } else {
+                // retrieve from the private variable if the intent is null
+
+                result = intent == null ? mCapturedImageURI : intent.getData();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mUploadMessage.onReceiveValue(result);
+        mUploadMessage = null;
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void handleUploadMessages(int requestCode, int resultCode, Intent intent) {
+        Uri[] results = null;
+        try {
+            if (resultCode != RESULT_OK) {
+                results = null;
+            } else {
+                if (intent != null) {
+                    String dataString = intent.getDataString();
+                    ClipData clipData = intent.getClipData();
+                    if (clipData != null) {
+                        results = new Uri[clipData.getItemCount()];
+                        for (int i = 0; i < clipData.getItemCount(); i++) {
+                            ClipData.Item item = clipData.getItemAt(i);
+                            results[i] = item.getUri();
+                        }
+                    }
+                    if (dataString != null) {
+                        results = new Uri[]{Uri.parse(dataString)};
+                    }
+                } else {
+                    results = new Uri[]{mCapturedImageURI};
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mUploadMessages.onReceiveValue(results);
+        mUploadMessages = null;
+    }
+
+
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+
+            view.loadUrl(url);
+            return true;
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    //flipscreen not loading again
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+    }
+
+// To handle "Back" key press event for WebView to go back to previous screen.
+/*@Override
+public boolean onKeyDown(int keyCode, KeyEvent event)
+{
+    if ((keyCode == KeyEvent.KEYCODE_BACK) && web.canGoBack()) {
+        web.goBack();
+        return true;
+    }
+    return super.onKeyDown(keyCode, event);
+}*/
+}
